547136211abd124d27d767c429c99e96b6b82c9d
Nazmul Haque <nazmulnjm@gmail.com>
2026-03-29 11:55:27 +0600

    updated login logo

--- resources/js/layouts/auth/AuthCardLayout.vue
+++ resources/js/layouts/auth/AuthCardLayout.vue
@@ -22,8 +22,8 @@ defineProps<{
                 <Card class="rounded-xl">
                     <CardHeader class="flex flex-col items-center justify-center gap-y-4 px-10 pt-8 pb-0 text-center">
                         <Link :href="home()" class="flex items-center gap-2 self-center font-medium">
-                            <div class="flex h-9 w-9 items-center justify-center">
-                                <AppLogoIcon class="size-9 fill-current text-black dark:text-white" />
+                            <div class="flex h-16 aspect-video items-center justify-center">
+                                <AppLogoIcon class=" fill-current text-black dark:text-white" />
                             </div>
                         </Link>
                         <CardTitle class="text-xl">{{ title }}</CardTitle>
